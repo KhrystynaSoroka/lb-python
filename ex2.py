4fdd46748c750dcb61df31806897e999311b7df0
KhrystynaSoroka <sorokakhristina05@gmail.com>
2024-11-11 20:18:00 +0200

    lb9

--- ex2.py
+++ ex2.py
@@ -1,6 +1,22 @@
 text = input("Введіть текстовий рядок: ")
-words = text.split()
-words_without_a = [word for word in words if 'а' not in word and 'А' not in word]
-print("Слова, які не містять літери 'а':")
-for word in words_without_a:
-    print(word)
+words = []
+for i in text.split():
+    if 'а' not in i.lower():  
+        words.append(i)
+
+numbers = []
+for word in words:
+    set1 = set()  
+    for char in word:
+        if char.isdigit(): 
+            set1.add(char) 
+    if set1:  
+        numbers.append(set1)
+
+if numbers:
+    intersection = set.intersection(*numbers)
+else:
+    intersection = set()
+
+print("Слова, які не містять літери 'а':", words)
+print("Перетин всіх множин цифр:", intersection)
--- ex3.py
+++ ex3.py
@@ -1,15 +1,30 @@
-text = input("Введіть текстовий рядок: ")
-words = text.split()
-max_count = 0
-words_max = []
-words=text.split()
-for i in words:
-    count_r = i.lower().count('r')
-    if count_r > max_count:
-        max_count = count_r
-        words_max = [i]  
-    elif count_r == max_count:
-        words_max.append(i)
-print("Слова, що містять максимальну кількість літер 'R':")
-for i in words_max:
-    print(i)
+flights = []
+n = int(input("Введіть кількість авіарейсів для додавання: "))
+
+for i in range(n):
+    print(f"\nВведіть інформацію про рейс {i + 1}:")
+    flight = {}
+    flight['flight_number'] = int(input("  Введіть номер рейсу (додатне ціле число): "))
+    flight['plane_name'] = input("  Введіть назву літака: ")
+    flight['destination'] = input("  Введіть місце призначення: ")
+    flight['total_seats'] = int(input("  Введіть кількість місць для пасажирів (шт., додатне ціле число): "))
+    flight['tickets_sold'] = int(input("  Введіть кількість проданих квитків (шт., додатне ціле число): "))
+    baggage = {}
+    baggage['type'] = input("  Введіть тип багажу: ")
+    baggage['total_weight'] = float(input("  Введіть загальну вагу багажу (в кг., додатне дійсне число): "))
+    baggage['estimated_value'] = int(input("  Введіть оціночну вартість багажу (в грн., додатне ціле число): "))
+    flight['baggage'] = baggage
+    flights.append(flight)
+
+print("\nІнформація про всі авіарейси:")
+for i, flight in enumerate(flights, start=1):
+    print(f"\nРейс {i}:")
+    print(f"  Номер рейсу: {flight['flight_number']}")
+    print(f"  Назва літака: {flight['plane_name']}")
+    print(f"  Місце призначення: {flight['destination']}")
+    print(f"  Кількість місць для пасажирів: {flight['total_seats']}")
+    print(f"  Кількість проданих квитків: {flight['tickets_sold']}")
+    print("  Інформація про багаж:")
+    print(f"    Тип багажу: {flight['baggage']['type']}")
+    print(f"    Загальна вага багажу: {flight['baggage']['total_weight']} кг")
+    print(f"    Оціночна вартість багажу: {flight['baggage']['estimated_value']} грн")
